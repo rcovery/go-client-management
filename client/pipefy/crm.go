c2b020f0a2b9d439560bb9a15f7a83f6da932e99
Ryan <ryanczpereira@gmail.com>
2026-05-28 02:25:38 -0300

    feat: adds CRM create card request

--- client/pipefy/crm.go
+++ client/pipefy/crm.go
@@ -1,7 +1,12 @@
 package pipefy
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/rcovery/go-client-management/client"
 )
@@ -17,16 +22,60 @@ func NewCRMGateway(pipefyToken string) *CRMGateway {
 }
 
 func (r *CRMGateway) CreateCard(ctx context.Context, clientData *client.Client) (bool, error) {
-	// _, execErr := r.DB.ExecContext(ctx, query,
-	// 	string(*clientData.ID),
-	// 	clientData.Name,
-	// 	clientData.Email,
-	// 	clientData.PortfolioValue,
-	// 	string(clientData.Status),
-	// )
-	// if execErr != nil {
-	// 	return false, execErr
-	// }
+	var (
+		pipeID = 123
+		title  = fmt.Sprintf("%s (%s)", clientData.Name, clientData.Email)
+	)
+
+	query := `
+		mutation CreateCard($pipeId: ID!, $title: String!, $fields: [FieldValueInput]!) {
+			createCard(input: {
+				pipe_id: $pipeId,
+				title: $title,
+				fields_attributes: $fields
+			}) {
+				card { title }
+			}
+		}
+	`
+	variables := map[string]any{
+		"pipeId": pipeID,
+		"title":  title,
+		"fields": []map[string]any{
+			{"field_id": "cliente_nome", "field_value": clientData.Name},
+			{"field_id": "cliente_email", "field_value": clientData.Email},
+			{"field_id": "valor_patrimonio", "field_value": clientData.PortfolioValue},
+		},
+	}
+
+	graphqlReqErr := r.runRequest(ctx, query, variables)
+	if graphqlReqErr != nil {
+		return false, graphqlReqErr
+	}
 
 	return true, nil
+}
+
+func (r *CRMGateway) runRequest(ctx context.Context, query string, variables map[string]any) error {
+	body, _ := json.Marshal(map[string]any{
+		"query":     query,
+		"variables": variables,
+	})
+
+	req, newReqErr := http.NewRequestWithContext(ctx, "POST", "https://api.pipefy.com/graphql", bytes.NewBuffer(body))
+	if newReqErr != nil {
+		log.Println(newReqErr.Error())
+		return fmt.Errorf("could.not.create.crm.request")
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+r.pipefyToken)
+
+	_, reqErr := http.DefaultClient.Do(req)
+	if reqErr != nil {
+		log.Println(reqErr.Error())
+		return fmt.Errorf("could.not.do.crm.request")
+	}
+
+	return nil
 }
